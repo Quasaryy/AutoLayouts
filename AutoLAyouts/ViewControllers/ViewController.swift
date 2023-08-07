3f14d66eef6850273b654d8b1dbf4031f35d5508
Yury Lebedev <quasaryy@gmail.comu>
2023-08-07 15:26:15 +0400

    All auto layout is ready with second modal screen and segues

---
+++ AutoLAyouts/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLAyouts
+//
+//  Created by Yury on 07/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
+
---
+++ AutoLAyouts/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherViewController.swift
+//  AutoLAyouts
+//
+//  Created by Yury on 07/08/2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func backTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
