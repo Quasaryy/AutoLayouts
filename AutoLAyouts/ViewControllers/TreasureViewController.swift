3f3c5146a682a6033051b90fe05c10e4be0f4ad0
Yury Lebedev <quasaryy@gmail.comu>
2023-08-09 23:20:17 +0400

    Added treasure map screen and ViewController

---
+++ AutoLAyouts/ViewControllers/TreasureViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TreasureViewController.swift
+//  AutoLAyouts
+//
+//  Created by Yury on 09/08/2023.
+//
+
+import UIKit
+
+class TreasureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func backTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func alsoBackTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+
+}
